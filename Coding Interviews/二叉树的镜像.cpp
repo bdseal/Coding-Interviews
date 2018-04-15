d2353142e496b3c1a567744aca12dc109597ed2b
bdseal <xiawh3@gmail.com>
2018-04-15 12:20:09 +0800

    update 二叉树的镜像.cpp

--- Coding Interviews/二叉树的镜像.cpp
+++ Coding Interviews/二叉树的镜像.cpp
@@ -7,7 +7,7 @@ struct TreeNode {
     TreeNode(int x) :
             val(x), left(NULL), right(NULL) {
     }
-};*/
+};
 class Solution {
 public:
     void Mirror(TreeNode *p) {
@@ -15,6 +15,40 @@ public:
             swap(p -> left, p -> right);
             Mirror(p -> left);
             Mirror(p -> right);
+        }
+    }
+};
+*/
+ 
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+        //递归实现
+        /*if(pRoot==NULL)
+            return;
+        TreeNode *ptemp=pRoot->left;
+        pRoot->left=pRoot->right;
+        pRoot->right=ptemp;
+        if(pRoot->left)
+            Mirror(pRoot->left);
+        if(pRoot->right)
+            Mirror(pRoot->right);*/
+        //非递归实现
+        if(pRoot==NULL) return;
+        stack<TreeNode*> stk;
+        stk.push(pRoot);
+        while(!stk.empty()){
+            TreeNode* tree=stk.top();
+            stk.pop();
+            if(tree->left!=NULL || tree->right!=NULL){
+                TreeNode *ptemp=tree->left;
+                tree->left=tree->right;
+                tree->right=ptemp;
+            }
+            if(tree->left)
+                stk.push(tree->left);
+            if(tree->right)
+                stk.push(tree->right);
         }
     }
 };
